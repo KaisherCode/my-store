aa8bf57ad6992cd493b085eabcd1d8f68780178d
Wilson Dev <wilson.hch@hynkor.com>
2023-07-22 16:16:40 -0500

    Códigos de estado o HTTP response codes

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -22,16 +22,22 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params
-  res.json({
-    id,
-    title: 'Red apple imported',
-    price: '8'
-  })
+  if(id==='999'){
+    res.status(404).json({
+      message:'not found'
+    })
+  }else{
+    res.status(200).json({
+      id,
+      title: 'Red apple imported',
+      price: '8'
+    })
+  }
 })
 
 router.post('/', (req, res) => {
   const body = req.body
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   })
